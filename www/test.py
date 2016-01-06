6eedaeb8325628d4cb56bb8b785159d813eb12c3
chenshunliang <imchensl@163.com>
2016-01-06 17:46:21 +0800

    test

--- www/test.py
+++ www/test.py
@@ -1,16 +1,30 @@
 import orm
 from models import User, Blog, Comment
 import asyncio
+import inspect
 
 
-async def test(loop):
-    await orm.create_pool(loop, user='chensl', password='123456', db='web_blog')
+# async def test(loop):
+#     await orm.create_pool(loop, user='chensl', password='123456', db='web_blog')
+#
+#     u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+#
+#     await u.save()
+#
+#
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(test(loop))
+# loop.run_forever()
 
-    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+def fun(name, chen=1, *args, **kw):
+    pass
 
-    await u.save()
 
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(test(loop))
-loop.run_forever()
+sig = inspect.signature(fun)
+t = (1,)
+print(type(sig))
+print(type(t))
+print(sig is tuple)
+for k, v in sig.parameters.items():
+    print(v.kind)
+print('....')
