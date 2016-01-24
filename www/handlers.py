b05613442b5453a210b2dcedc77c389cb367b76a
chenshunliang <imchensl@163.com>
2016-01-24 23:51:40 +0800

    日志编辑 创建

--- www/handlers.py
+++ www/handlers.py
@@ -148,3 +148,38 @@ async def authenticate(*, email, passwd):
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
+
+
+@get('/signout')
+def signout(request):
+    referer = request.headers.get('Referer')
+    r = web.HTTPFound(referer or 'r')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
+    logging.info('user signed out')
+    return r
+
+
+@get('/manage/blogs/create')
+def manage_blogs_create():
+    return {'__template__': 'manage_blogs_edit.html', 'id': '', 'action': '/api/blogs'}
+
+
+@get('/api/blogs/{id}')
+async def api_get_log(*, id):
+    blog = await Blog.find(id)
+    return blog
+
+
+@post('/api/blogs')
+async def api_create_blog(request, *, name, summary, content):
+    check_admin(request)
+    if not name or not name.strip():
+        raise APIValueError('name', 'name cannot be empty')
+    if not summary or not summary.strip():
+        raise APIValueError('summary', 'summary cannot be empty')
+    if not content or not content.strip():
+        raise APIValueError('content', 'content cannot be empty')
+    blog = Blog(user_id=request.__user__.id, user_name=request.__user__.name, user_image=request.__user__.image,
+                name=name.strip(), summary=summary.strip(), content=content.strip())
+    await blog.save()
+    return blog
