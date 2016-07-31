3fb72950904417c00b6bbdd024214268ef05feb6
chenshunliang <imchensl@163.com>
2016-07-31 23:02:08 +0800

    sublime脚本

--- djangor/djr/tornado/sublime.py
+++ djangor/djr/tornado/sublime.py
@@ -1,8 +1,24 @@
 import os
 import urllib.request
 
-pf = 'Package Control.sublime-package'
-ipp = '/Users/chenshunliang/Library/Application Support/Sublime Text 3/Installed Packages'
-urllib.request.install_opener(urllib.request.build_opener(urllib.request.ProxyHandler()))
-open(os.path.join(ipp, pf), 'wb').write(
-    urllib.request.urlopen('http://sublime.wbond.net/' + pf.replace(' ', '%20')).read())
+
+#
+# pf = 'Package Control.sublime-package'
+# ipp = '/Users/chenshunliang/Library/Application Support/Sublime Text 3/Installed Packages'
+# urllib.request.install_opener(urllib.request.build_opener(urllib.request.ProxyHandler()))
+# open(os.path.join(ipp, pf), 'wb').write(
+# urllib.request.urlopen('http://sublime.wbond.net/' + pf.replace(' ', '%20')).read())
+
+
+def extendList(val, list=[]):
+    list.append(val)
+    return list
+
+
+list1 = extendList(10)
+list2 = extendList(123, [])
+list3 = extendList('a')
+
+print("list1 = %s" % list1)
+print("list2 = %s" % list2)
+print("list3 = %s" % list3)
